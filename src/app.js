ebcdf53828e88e39c24d7707bf8aaf4022cb8630
Queeneth-dev <okekequeenethi@gmail.com>
2023-11-05 19:16:07 +0100

    updated  my javascript icon

--- src/app.js
+++ src/app.js
@@ -8,24 +8,53 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 function searchCity(city) {
-  let apiKey = "470eca45f77c5d6f6d2cdffd8f474b95";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiKey = "64214f5tefe70fb5f0ob40f3f9673d4a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showWeather);
 }
 
 function showWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
-  let temperature = response.data.main.temp;
+  let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let timeElement = document.querySelector("#time");
+  let date = new Date();
+  let iconElement = document.querySelector("#icon");
 
-  cityElement.innerHTML = response.data.name;
-  descriptionElement.innerHTML = response.data.weather[0].main;
+  cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = dateToday(date);
+  descriptionElement.innerHTML = response.data.condition.description;
   temperatureElement.innerHTML = Math.round(temperature);
-  humidityElement.innerHTML = `${response.data.main.humidity}%`;
-  windElement.innerHTML = Math.round(response.data.wind.speed);
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${Math.round(response.data.wind.speed)}km/h`;
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}"class= "weather-app-icon"/>`;
+}
+
+function dateToday(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 searchCity("Paris");
